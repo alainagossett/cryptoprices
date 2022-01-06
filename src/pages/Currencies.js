9e525ac487a023e883e669b0a8d143b8a4f808b5
Alaina <alaina.gossett@gmail.com>
2022-01-05 17:28:49 -0700

    adds pages boilerplates

---
+++ src/pages/Currencies.js
@@ -0,0 +1,5 @@
+const Currencies = (props) => {
+    return <h1>This is the currencies page</h1>
+};
+
+export default Currencies;
---
+++ src/pages/Price.js
@@ -0,0 +1,5 @@
+const Price = (props) => {
+    return <h1>This is the Price Page</h1>
+};
+
+export default Price;
